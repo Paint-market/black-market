47a07f6c60f06a03123c50062e171d725ed8eb16
Nicholas r Maskell <nick@Nicholass-MacBook-Pro.local>
2016-03-10 20:24:16 +1300

    Route pulling dumby data

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,22 @@
 var fs = require('fs')
-var app = require('express')
 var path = require('path')
+var Router = require('express').Router
+var bodyParser = require('body-parser')
+var router = Router()
 
-module.exports = function () {
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-  app.get('/market/paintings', function (req, res) {
-    console.log("REQ", req.body)
-    fs.readFile(join.path(__dirname, 'db.json'), function (resp){
-      
+  router.get('/', function (req, res) {
+    res.send('Hello Gay')
+  })
+
+  router.get('/market/paintings', urlencodedParser, function (req, res) {
+    // console.log("REQ", req)
+    var jsonPath = path.join(__dirname, '../data/db.json')
+    console.log(jsonPath)
+    fs.readFile(jsonPath, 'utf8', function (err, data){
+        console.log(data)
     })
   })
 
-  
-}
+module.exports = router
